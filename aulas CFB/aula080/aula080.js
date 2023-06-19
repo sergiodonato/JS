1c1e0ef44eefa3591c703d05ff84abb09479e1ed
Sergio Donato Camacho do Nascimento <sergiodcdn@gmail.com>
2023-06-19 16:48:46 -0300

    up81

---
+++ aulas CFB/aula080/aula080.js
@@ -0,0 +1,12 @@
+// Aprendendo sobre a biblioteca MATH:
+
+const mat = document.getElementById("mat")
+
+// const num = Math.random()*10
+// const num = Math.floor(Math.random()*10) + 1 // gera entre 1 e 10
+
+// mat.innerHTML = Math.round(num)
+
+// mat.innerHTML = Math.sqrt(15)
+
+mat.innerHTML = Math.pow(5, 3)
