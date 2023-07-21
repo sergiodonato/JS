00ebc5e10b863840af8cc3878598700242f72217
Sergio Donato <sergiodcdn@gmail.com>
2023-07-21 16:14:03 -0300

    boleano básico com saida pro html

--- mdn docs/index.js
+++ mdn docs/index.js
@@ -1,8 +1,9 @@
-var button = document.querySelector('button');
-const teste = document.querySelector('.teste')
 
-button.onclick = function() {
-  var nome = prompt('Qual é o seu nome?');
-  alert('Olá ' + nome + ', é um prazer te ver!');
-  teste.textContent = nome
-}
+
+var myString = '123';
+var myNum = Number(myString);
+console.log(typeof(myNum));
+
+var myNum = 123;
+var myString = myNum.toString();
+console.log(typeof(myString));
